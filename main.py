1f033eb58c75c7519905745f8a10c8aa0864d948
stanoelle <118582341+stanoelle@users.noreply.github.com>
2023-05-25 10:24:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ from pydrive.auth import GoogleAuth
 
 from pydrive.drive import GoogleDrive
 
+# Get magnet URL from user
 
-magnet_url = ""
+magnet_url = input("Enter Magnet URL: ")
+
+# Specify the save path for downloaded files
+
+save_path = '/content/Downloads/'  # Set the save path to the "Downloads" directory
 
 # Download torrent file using libtorrent
 
@@ -15,11 +20,17 @@ ses = lt.session()
 
 params = {
 
-    'save_path': '/Downloads/',
+    'save_path': save_path,
 
-    'storage_mode': lt.storage_mode_t(2)
+    'storage_mode': lt.storage_mode_t(2),
 
-}    
+    'paused': False,
+
+    'auto_managed': True,
+
+    'duplicate_is_error': True
+
+}
 
 handle = lt.add_magnet_uri(ses, magnet_url, params)
 
@@ -49,30 +60,59 @@ while handle.status().state != lt.torrent_status.seeding:
 
 gauth = GoogleAuth()
 
-# Set the client ID and client secret
-
-gauth.client_config = {
-
-    "client_id": "1123530391775-vmcgl1seh9tmh48qrkvh6oga0mhd89qc.apps.googleusercontent.com",
-
-    "client_secret": "AGOCSPX--HNcTAqpCyDfuSwkM8mzVJquLCxt",
-
-    "redirect_uri": "https://developers.google.com/oauthplayground"
-
-}
-
-# Set the access token
-
-gauth.credentials.refresh_token = "1//04_NxCGKZv736CgYIARAAGAQSNwF-L9IrsGPwuTqIMEMfiVWfKyoWTJBlj9iErueQ0uc1pw3w2sNytQOvo5P7J0RpRcGmomlKCFM"
-
 drive = GoogleDrive(gauth)
 
-# Upload file to Google Drive
+# Define the name of the folder
 
-file1 = drive.CreateFile({'title': 'Downloaded Torrent File'})
+folder_name = 'Movies'
 
-file1.Upload()
+# Check if the folder exists in Google Drive
 
-print("File has been uploaded")
+existing_folders = drive.ListFile({'q': "title='" + folder_name + "' and mimeType='application/vnd.google-apps.folder' and trashed=false"}).GetList()
 
-                                                                                          
+if len(existing_folders) > 0:
+
+    folder = existing_folders[0]
+
+    folder_id = folder['id']
+
+else:
+
+    # Create the folder in Google Drive
+
+    folder_metadata = {'title': folder_name, 'mimeType': 'application/vnd.google-apps.folder'}
+
+    folder = drive.CreateFile(folder_metadata)
+
+    folder.Upload()
+
+    folder_id = folder['id']
+
+# Get a list of files in the save_path directory
+
+files = os.listdir(save_path)
+
+# Upload each file to Google Drive
+
+for file_name in files:
+
+    file_path = os.path.join(save_path, file_name)
+
+    # Create a file in Google Drive and set its properties
+
+    file_drive = drive.CreateFile({
+
+        'title': file_name,
+
+        'parents': [{'id': folder_id}]
+
+    })
+
+    file_drive.SetContentFile(file_path)
+
+    file_drive.Upload()
+
+    print(f"File '{file_name}' has been uploaded to Google Drive.")
+
+print("All files have been uploaded to Google Drive.")
+
