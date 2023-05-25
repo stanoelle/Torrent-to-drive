2afa4a0ac5747475c7f5fa23339efba3c211bc0d
stanoelle <118582341+stanoelle@users.noreply.github.com>
2023-05-25 19:40:53 +0530

    Update main.py
    Fixed

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 import libtorrent as lt
-
 import os
-
 from pydrive.auth import GoogleAuth
-
 from pydrive.drive import GoogleDrive
 
-# Get magnet URL from user
 
-magnet_url = input("Enter Magnet URL: ")
-
-# Specify the save path for downloaded files
-
-save_path = '/content/Downloads/'  # Set the save path to the "Downloads" directory
+magnet_url = input("Enter Magnet URL: ").strip()
+save_path = 'C:/Downloads/'  
 
 # Download torrent file using libtorrent
 
@@ -23,19 +16,10 @@ params = {
     'save_path': save_path,
 
     'storage_mode': lt.storage_mode_t(2),
-
-    'paused': False,
-
-    'auto_managed': True,
-
-    'duplicate_is_error': True
-
 }
 
 handle = lt.add_magnet_uri(ses, magnet_url, params)
-
 ses.start_dht()
-
 print("Downloading metadata...")
 
 while not handle.has_metadata():
@@ -59,15 +43,8 @@ while handle.status().state != lt.torrent_status.seeding:
 # Authenticate with Google Drive using PyDrive
 
 gauth = GoogleAuth()
-
 drive = GoogleDrive(gauth)
-
-# Define the name of the folder
-
 folder_name = 'Movies'
-
-# Check if the folder exists in Google Drive
-
 existing_folders = drive.ListFile({'q': "title='" + folder_name + "' and mimeType='application/vnd.google-apps.folder' and trashed=false"}).GetList()
 
 if len(existing_folders) > 0:
@@ -88,7 +65,6 @@ else:
 
     folder_id = folder['id']
 
-# Get a list of files in the save_path directory
 
 files = os.listdir(save_path)
 
@@ -96,7 +72,7 @@ files = os.listdir(save_path)
 
 for file_name in files:
 
-    file_path = os.path.join(save_path, file_name)
+    file_path = os.path.join(save_path, file_name).replace('/', '\\')  # Modify the file path separator for Windows
 
     # Create a file in Google Drive and set its properties
 
@@ -115,4 +91,3 @@ for file_name in files:
     print(f"File '{file_name}' has been uploaded to Google Drive.")
 
 print("All files have been uploaded to Google Drive.")
-
